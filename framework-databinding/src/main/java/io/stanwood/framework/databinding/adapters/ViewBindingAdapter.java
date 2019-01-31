a058d90fd9740a95086ef979aa08f0e11f735f35
andre <andreclassen1337@gmail.com>
2019-02-01 00:22:37 +0100

    Cleanup ui package
    Upgrade ui package to androidX

--- framework-databinding/src/main/java/io/stanwood/framework/databinding/adapters/ViewBindingAdapter.java
+++ framework-databinding/src/main/java/io/stanwood/framework/databinding/adapters/ViewBindingAdapter.java
@@ -1,20 +1,21 @@
 package io.stanwood.framework.databinding.adapters;
 
 import android.databinding.BindingAdapter;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ViewBindingAdapter {
     @BindingAdapter(value = {"android:visibility"})
-    public static void setVisibility(View view, boolean visible) {
-        view.setVisibility(visible ? View.VISIBLE : View.GONE);
+    public static void setVisibility(View view, @Nullable Boolean visible) {
+        view.setVisibility((visible != null && visible) ? View.VISIBLE : View.GONE);
     }
 
     /**
      * A binding adapter which sets the layout_width to wrap_content if the condition is met.
      *
-     * @param view the View this adapter is applied to
-     * @param width the width if the condition is not met
+     * @param view      the View this adapter is applied to
+     * @param width     the width if the condition is not met
      * @param condition if {@code true} the width will be set to wrap_content, if {@code false} the value of the width parameter is used
      */
     @BindingAdapter(value = {"android:layout_width", "layout_width_wrap_content_condition"})
@@ -33,8 +34,8 @@ public class ViewBindingAdapter {
     /**
      * A binding adapter which sets the layout_height to wrap_content if the condition is met.
      *
-     * @param view the View this adapter is applied to
-     * @param height the height if the condition is not met
+     * @param view      the View this adapter is applied to
+     * @param height    the height if the condition is not met
      * @param condition if {@code true} the height will be set to wrap_content, if {@code false} the value of the height parameter is used
      */
     @BindingAdapter(value = {"android:layout_height", "layout_height_wrap_content_condition"})
@@ -53,8 +54,8 @@ public class ViewBindingAdapter {
     /**
      * A binding adapter which sets the layout_width to match_parent if the condition is met.
      *
-     * @param view the View this adapter is applied to
-     * @param width the width if the condition is not met
+     * @param view      the View this adapter is applied to
+     * @param width     the width if the condition is not met
      * @param condition if {@code true} the width will be set to match_parent, if {@code false} the value of the width parameter is used
      */
     @BindingAdapter(value = {"android:layout_width", "layout_width_match_parent_condition"})
@@ -73,8 +74,8 @@ public class ViewBindingAdapter {
     /**
      * A binding adapter which sets the layout_height to match_parent if the condition is met.
      *
-     * @param view the View this adapter is applied to
-     * @param height the height if the condition is not met
+     * @param view      the View this adapter is applied to
+     * @param height    the height if the condition is not met
      * @param condition if {@code true} the height will be set to match_parent, if {@code false} the value of the height parameter is used
      */
     @BindingAdapter(value = {"android:layout_height", "layout_height_match_parent_condition"})
