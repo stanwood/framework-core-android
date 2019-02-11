bbaa0aeb80909bd8f0a48668d4d433633dfe8c23
andre <andreclassen1337@gmail.com>
2019-02-11 02:53:53 +0100

    Refactored view binding adapter

--- databinding/src/main/java/io/stanwood/framework/databinding/adapters/ViewBindingAdapter.kt
+++ databinding/src/main/java/io/stanwood/framework/databinding/adapters/ViewBindingAdapter.kt
@@ -3,16 +3,16 @@ package io.stanwood.framework.databinding.adapters
 import android.view.View
 import androidx.databinding.BindingAdapter
 
-object ViewBindingAdapter {
-    @BindingAdapter("android:visibility")
-    @JvmStatic
-    fun setVisibileOrGone(view: View, value: Boolean?) {
-        view.visibility = if (value == true) View.VISIBLE else View.GONE
+@set:BindingAdapter("android:visibility")
+var View.visibleOrGone
+    get() = visibility == View.VISIBLE
+    set(value) {
+        visibility = if (value) View.VISIBLE else View.GONE
     }
 
-    @BindingAdapter("visibleOrInvisible")
-    @JvmStatic
-    fun setVisibleOrInvisible(view: View, value: Boolean?) {
-        view.visibility = if (value == true) View.VISIBLE else View.INVISIBLE
+@set:BindingAdapter("visible")
+var View.visibleOrInvisible
+    get() = visibility == View.VISIBLE
+    set(value) {
+        visibility = if (value) View.VISIBLE else View.INVISIBLE
     }
-}
